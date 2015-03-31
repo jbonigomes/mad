a026db763595320ff0259d0087c913646c1777d3
Jose B Gomes <jbonigomes@yahoo.com.br>
2015-03-31 02:44:35 +0100

    Monday changes

--- fma/implementation/js/main.js
+++ fma/implementation/js/main.js
@@ -14,74 +14,153 @@ $(document).on('pagecontainerbeforeshow', function(e, ui) {
 
   if(page == 'map') {
     if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(initialize);
+
+      var config = {
+        lat: 51.52307,
+        long: -0.12426,
+        currPosImg: '/img/currentLocation.png',
+        pinPosImg: '/img/mapPin.png',
+        mapElement: 'contact-map-canvas',
+        currPosTxt: 'You are here',
+        markerPosTxt: 'Zedland University',
+        infobox: {
+          title: 'Zedland University',
+          content: 'Welcome to Zedland, home of the Zedland University',
+          url: 'http://www.bbk.ac.uk',
+          linktext: 'Details'
+        }
+      };
+
+      initialise(config);
+
     } else {
+
       $('#nogeolocation')
         .innerHTML = 'Geolocation is not supported by this browser.';
     }
   }
 
-  if(page == 'accommodation') {
-    $.get('data/accommodation.json', function(result, status) {
+  if(page == 'contact') {
+    if(navigator.geolocation) {
+      
+      var config = {
+        lat: 51.52307,
+        long: -0.12426,
+        currPosImg: '/img/currentLocation.png',
+        pinPosImg: '/img/mapPin.png',
+        mapElement: 'contact-map-canvas',
+        currPosTxt: 'You are here',
+        markerPosTxt: 'Zedland University',
+        infobox: {
+          title: 'Zedland University',
+          content: 'Welcome to Zedland, home of the Zedland University',
+          url: 'http://www.bbk.ac.uk',
+          linktext: 'Details'
+        }
+      };
+
+      initialise(config);
+
+    } else {
+
+      $('#nogeolocation')
+        .innerHTML = 'Geolocation is not supported by this browser.';
+    }
+  }
+
+  if(page == 'accommodationlist') {
+    $.get('data/accommodation.json', function(results) {
       var accommodation = "";
       
-      for(var i = 0; i < result.length; i++) {
+      results.forEach(function(result) {
         accommodation += '' +
           '<li>' +
-            '<a href="hotel.html?id=' + result[i].id + '" class="ui-alt-icon">' +
-              '<img src="/img/locations/thumb/accommodation_' + result[i].id + '.jpg">' +
-              '<h3>' + result[i].name + '</h3>' +
+            '<a href="accommodation.html?id=' + result.id + '" class="ui-alt-icon">' +
+              '<img src="/img/locations/thumb/accommodation_' + result.id + '.jpg">' +
+              '<h3>' + result.name + '</h3>' +
               '<p>' +
-                getStars(result[i].stars) + '<br/>' +
+                getStars(result.stars) + '<br/>' +
                 '<span class="left">' +
-                  result[i].type + ' @ ' + result[i].location +
+                  result.type + ' @ ' + result.location +
                 '</span>' +
-                '<span class="right">&pound;' + result[i].price + 'pw</span>' +
+                '<span class="right">&pound;' + result.price + 'pw</span>' +
               '</p>' +
             '</a>' +
           '</li>';
-      }
+      });
 
-      $('#accommodationlist').html(accommodation).listview('refresh');
+      $('#accommodationlistitems').html(accommodation).listview('refresh');
 
     }, 'json');
   }
 
-  if(page == 'hotelpage') {
+  if(page == 'accommodation') {
+
     var thisPage = "#" + page;
-    var thisUrl = $(location).attr('search');
-    var thisId = thisUrl.split("=")[1];
+    var thisUrl  = $(location).attr('search');
+    var thisId   = thisUrl.split("=")[1];
 
-    $.get('data/hotel' + thisId +'.json', function(result, status) {
-      $('#hoteltitle',thisPage).text(result.name);
-      $('#hotelid', thisPage).html(thisId);
-      var hotel = '<p>' + result.stars + '</p><p>' + result.description + '</p>';
-      $('#contentArea', thisPage).html(hotel);
+    $.get('data/accommodation.json', function(results) {
 
-      // Add the favourites button
+      var result = getAccommodationById(results, thisId);
+
+      $('body').on('click', '#addbtn', function() {
+
+        if(typeof(Storage) != 'undefined') {
+
+          if($(this).hasClass('active')) {
+            removeDetails(thisId);
+            $(this).removeClass('active');
+          } else {
+            setDetails(result, thisId);
+            $(this).addClass('active');
+          }
+        } else {
+          $(this).addClass('ui-btn-hidden');
+          $('#nostorage').text('Local storage not supported');
+        }
+      });
+
+      var accommodation = '' +
+        '<p>' + getStars(result.stars) + '</p>' +
+        '<p>' +
+          '<span>' + result.type + ' @ ' + result.location + '</span>' +
+          result.description +
+          '<span>&pound' + result.price + 'pw</span>' +
+        '</p>';
+
+      $('#contentArea', thisPage).html(accommodation);
+      $('#accommodationtitle', thisPage).text(result.name);
+
       var active = '';
 
-      if(isFavourite()) {
+      console.log(isFavourite(thisId));
+
+      if(isFavourite(thisId)) {
         active = 'active';
       }
 
-      var favouritesButton = '<a id="addbtn" href="#" class="ui-btn ui-icon-star ' +
-        'ui-btn-icon-left ui-corner-all ' + active + '">Favourites</a>';
+      var favouritesButton = '' +
+        '<a id="addbtn" ' +
+           'href="#" ' +
+           'class="ui-btn ui-icon-star ui-btn-icon-left ui-corner-all '+active+'">' +
+          'Favourites'
+        '</a>';
 
       $('.favourites').html(favouritesButton);
 
     }, 'json');
   }
   
-  if(page === 'myhotels') {
+  if(page === 'favourites') {
     if(typeof(Storage) !== undefined) {
-      displayHotelDetails(getHotelDetails());
+      displayAccommodationDetails(getDetails());
     } else {
       $('#nostorage').text('Local storage not supported');
     }
   }
 
-  // Catch login form submit
+  // Catch login form submition
   $('#frm1').on('submit', function(e) {
     e.preventDefault();
     if($(this).valid()) {
@@ -89,7 +168,7 @@ $(document).on('pagecontainerbeforeshow', function(e, ui) {
     }
   });
 
-  // Catch booking form submit
+  // Catch booking form submition
   $('#frm2').on('submit', function(e) {
     e.preventDefault();
     if($(this).valid()) {
@@ -97,7 +176,7 @@ $(document).on('pagecontainerbeforeshow', function(e, ui) {
     }
   });
 
-  // Catch lost password form submit
+  // Catch lost password form submition
   $('#frm3').on('submit', function(e) {
     e.preventDefault();
     if($(this).valid()) {
@@ -105,28 +184,44 @@ $(document).on('pagecontainerbeforeshow', function(e, ui) {
     }
   });
 
-  // Init form validation
+  // Validate forms
   $('#frm1').validate();
   $('#frm2').validate();
   $('#frm3').validate();
 
-  // Add to favourites action
-  $('body').on('click', '#addbtn', function() {
+  function getAccommodationById(results, id) {
+    
+    var result = null;
 
-    if(typeof(Storage) != 'undefined') {
-
-      if($(this).hasClass('active')) {
-        removeDetails(getTitle());
-        $(this).removeClass('active');
-      } else {
-        setDetails(getTitle(), getUrl());
-        $(this).addClass('active');
+    results.forEach(function(item) {
+      if(item.id == id) {
+        result = item;
       }
+    });
+
+    return result;
+  }
+
+  function displayAccommodationDetails(details) {
+
+    if(details !== null) {
+
+      var accommodation = '';
+
+      details.forEach(function(detail) {
+        accommodation += '' +
+          '<a href="accommodation.html?id=' + detail.id + '"' +
+          '   class="ui-btn ui-corner-all ui-icon-arrow-r ui-btn-icon-right">' +
+              detail.name +
+          '</a>';
+      });
+
+      $('#favouriteslist').html(accommodation);
+
     } else {
-      $(this).addClass('ui-btn-hidden');
-      $('#nostorage').text('Local storage not supported');
+      $('#nostorage').html('You do not have any accommodation in your list yet');
     }
-  });
+  }
 
   function getStars(number) {
 
@@ -146,13 +241,13 @@ $(document).on('pagecontainerbeforeshow', function(e, ui) {
     return html;
   }
 
-  function isFavourite() {
-    var exists       = false;
-    var url          = getUrl();
-    var storedHotels = getHotelDetails() || [];
+  function isFavourite(id) {
 
-    storedHotels.forEach(function(hotel) {
-      if(hotel.hotelurl == url) {
+    var exists = false;
+    var storedAccommodation = getDetails() || [];
+
+    storedAccommodation.forEach(function(accommodation) {
+      if(accommodation.id == id) {
         exists = true;
       }
     });
@@ -160,137 +255,253 @@ $(document).on('pagecontainerbeforeshow', function(e, ui) {
     return exists;
   }
 
-  function getTitle() {
-    return $('#hoteltitle').text();
-  }
+  function setDetails(accommodation, id) {
 
-  function getUrl() {
-    return getTitle().replace(/\s+/g, '-').toLowerCase();
-  }
+    var accommodationExist  = false;
+    var storedAccommodation = getDetails() || [];
 
-  function setDetails(title, url) {
-    // Create our hotel object
-    var hotel = {
-      name: title,
-      hotelurl: url
-    };
-
-    // Let's assume this hotel object does not exist in our local storage
-    var thisHotelExist = false;
-
-    // Get local storage
-    var storedHotels = getHotelDetails() || [];
-
-    // Check if the hotel already exist in local storage
-    storedHotels.forEach(function(currHotel) {
-      if(currHotel.name === hotel.name) {
-        thisHotelExist = true;
+    storedAccommodation.forEach(function(currAccommodation) {
+      if(currAccommodation.name === accommodation.name) {
+        accommodationExist = true;
       }
     });
 
-    // Only add to local storage if the hotel is not already there
-    if(!thisHotelExist) {
-      storedHotels.push(hotel);
-      localStorage.setItem('hotels', JSON.stringify(storedHotels));
+    if(!accommodationExist) {
+      storedAccommodation.push(accommodation);
+      localStorage.setItem('accommodation', JSON.stringify(storedAccommodation));
     }
   }
 
-  function removeDetails(title) {
+  function removeDetails(id) {
 
-    // Get local storage
-    var storedHotels = getHotelDetails() || [];
+    var storedAccommodation    = getDetails() || [];
+    var newStoredAccommodation = [];
 
-    // Set an empty new list
-    var newStoredHotels = [];
-
-    storedHotels.forEach(function(currHotel) {
-      if(currHotel.name !== title) {
-        newStoredHotels.push(currHotel);
+    storedAccommodation.forEach(function(currAccommodation) {
+      if(currAccommodation.id !== id) {
+        newStoredAccommodation.push(currAccommodation);
       }
     });
 
-    localStorage.setItem('hotels', JSON.stringify(newStoredHotels));
+    localStorage.setItem('accommodation', JSON.stringify(newStoredAccommodation));
   }
 
-  function getHotelDetails() {
-    var hotelDetails = JSON.parse(localStorage.getItem('hotels'));
-    return hotelDetails;
+  function getDetails() {
+    return JSON.parse(localStorage.getItem('accommodation'));
   }
 
-  function displayHotelDetails(details) {
+  function initialise(config) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
 
-    if(details !== null) {
+      var currentPosition = new google.maps.LatLng(lat, lon);
+      var zedlandPosition = new google.maps.LatLng(config.lat, config.long);
 
-      var hotel = '';
+      var currentPositionImage = config.currPosImg;
+      var zedlandPositionImage = config.pinPosImg;
 
-      details.forEach(function(detail) {
-        hotel += '' +
-          '<a href="' + detail.hotelurl + '.html"' +
-             'class="ui-btn ui-corner-all ui-icon-arrow-r ui-btn-icon-right">' +
-              detail.name +
-          '</a>';
+      var mapOptions = {
+        zoom: 13,
+        center: zedlandPosition,
+      };
+
+      var mapstyles = getMapStyles();
+
+      var target = document.getElementById(config.mapElement);
+      var mapObj = new google.maps.Map(target, mapOptions);
+
+      var userPosition = makeMarker({
+        position: currentPosition,
+        map: mapObj,
+        icon: currentPositionImage,
+        title: config.currPosTxt
       });
 
-      $('#hotel').html(hotel);
+      var zedlanPosition = makeMarker({
+        position: zedlandPosition,
+        map: mapObj,
+        icon: zedlandPositionImage,
+        title: config.markerPosTxt
+      });
 
-    } else {
-      $('#nostorage').html('You do not have any hotels in your list yet');
-    }
+      var zedlandInfoBox = makeInfoBox({
+        title: config.infobox.title,
+        content: config.infobox.content,
+        url: config.infobox.url,
+        linktext: config.infobox.linktext
+      });
+
+      mapObj.setOptions({styles: mapstyles});
+
+      google.maps.event.addListener(zedlanPosition, 'click', function() {
+        zedlandInfoBox.open(mapObj, zedlanPosition);
+      });
+    });
   }
 
+  function makeInfoBox(config) {
+    var html = '' +
+      '<div id="mappopup">' +
+        '<h4>' + config.title + '</h4>' +
+        '<p>' + config.content + '</p>' +
+        '<a href="' + config.url + '">' + config.linktext + '</a>' +
+      '</div>';
+
+    return new google.maps.InfoWindow({
+      content: html
+    });
+  }
+
+  function makeMarker(config) {
+    return new google.maps.Marker({
+      position: config.position,
+      map: config.map,
+      icon: config.icon,
+      title: config.title
+    });
+  }
+
+  function getMapStyles() {
+    return [
+      {
+        "featureType":"landscape.man_made",
+        "elementType":"geometry.fill",
+        "stylers":[
+          {
+            "color":"#e9e5dc"
+          }
+        ]
+      },
+      {
+        "featureType":"landscape.natural",
+        "elementType":"geometry.fill",
+        "stylers":[
+          {
+            "visibility":"on"
+          },
+          {
+            "color":"#b8cb93"
+          }
+        ]
+      },
+      {
+        "featureType":"poi",
+        "elementType":"all",
+        "stylers":[
+          {
+            "visibility":"off"
+          }
+        ]
+      },
+      {
+        "featureType":"poi.business",
+        "elementType":"all",
+        "stylers":[
+          {
+            "visibility":"simplified"
+          }
+        ]
+      },
+      {
+        "featureType":"poi.medical",
+        "elementType":"all",
+        "stylers":[
+          {
+            "visibility":"on"
+          }
+        ]
+      },
+      {
+        "featureType":"poi.park",
+        "elementType":"all",
+        "stylers":[
+          {
+            "visibility":"on"
+          }
+        ]
+      },
+      {
+        "featureType":"poi.park",
+        "elementType":"geometry.fill",
+        "stylers":[
+          {
+            "color":"#ccdca1"
+          }
+        ]
+      },
+      {
+        "featureType":"poi.sports_complex",
+        "elementType":"all",
+        "stylers":[
+          {
+            "visibility":"on"
+          }
+        ]
+      },
+      {
+        "featureType":"road",
+        "elementType":"geometry.fill",
+        "stylers":[
+          {
+            "hue":"#ff0000"
+          },
+          {
+            "saturation":-100
+          },
+          {
+            "lightness":99
+          }
+        ]
+      },
+      {
+        "featureType":"road",
+        "elementType":"geometry.stroke",
+        "stylers":[
+          {
+            "color":"#808080"
+          },
+          {
+            "lightness":54
+          },
+          {
+            "visibility":"off"
+          }
+        ]
+      },
+      {
+        "featureType":"road",
+        "elementType":"labels.text.fill",
+        "stylers":[
+          {
+            "color":"#767676"
+          }
+        ]
+      },
+      {
+        "featureType":"road",
+        "elementType":"labels.text.stroke",
+        "stylers":[
+          {
+            "color":"#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType":"water",
+        "elementType":"all",
+        "stylers":[
+          {
+            "saturation":43
+          },
+          {
+            "lightness":-11
+          },
+          {
+            "color":"#89cada"
+          }
+        ]
+      }
+    ];
+  }
 });
-
-// Inititialize map (has to be global [don't know why])
-function initialize(position) {
-
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-
-  var currentPosition        = new google.maps.LatLng(lat, lon);
-  var churchillHotelPosition = new google.maps.LatLng(51.52307,-0.12426);
-
-  var mapOptions = {
-    zoom: 12,
-    center: currentPosition,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-    }
-  };
-
-  var target   = document.getElementById('map-canvas');
-  var hotelMap = new google.maps.Map(target, mapOptions);
-
-  var currentPositionImage = 'http://goo.gl/JGqmEh';
-
-  var userPosition = new google.maps.Marker({
-    position: currentPosition,
-    map: hotelMap,
-    icon: currentPositionImage,
-    title: 'You are here'
-  });
-
-  var churchillHotelMarkerImage = 'http://goo.gl/3UKr2k';
-
-  var churchillPosition = new google.maps.Marker({
-    position: churchillHotelPosition,
-    map: hotelMap,
-    icon: churchillHotelMarkerImage,
-    title: 'Churchill Hotel'
-  });
-
-  var churchillHotelInfo = '' +
-    '<div id="mappopup">' +
-      '<h4>Churchill Hotel</h4>' +
-      '<p>Five Star Hotel in the middle of capital city</p>' +
-      '<a href="churchill-hotel.html">Details</a>' +
-    '</div>';
-
-  var churchillHotelInfoWindow = new google.maps.InfoWindow({
-    content: churchillHotelInfo
-  });
-
-  google.maps.event.addListener(churchillPosition, 'click', function() {
-    churchillHotelInfoWindow.open(hotelMap, churchillPosition);
-  });
-}
